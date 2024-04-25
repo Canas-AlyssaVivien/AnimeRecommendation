c09206b8816f0acef4cf0c7bce0c1c0c18a94397
Alyssa Vivien S. Cañas <alyssavivien.sc@gmail.com>
2024-04-25 13:06:36 +0800

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,14 @@ from keras.models import load_model
 
 model = load_model('anime_recommendation_model.h5')
 
-with open('preprocessing_pipeline.pkl', 'rb') as f:
-    preprocessing_pipeline = pickle.load(f)
+#with open('preprocessing_pipeline.pkl', 'rb') as f:
+#    preprocessing_pipeline = pickle.load(f)
 
-def preprocess_data(input_data):
-    return preprocessing_pipeline.transform(input_data)
+#def preprocess_data(input_data):
+#    return preprocessing_pipeline.transform(input_data)
+
+from preprocessor import Preprocessor
+preprocessor = Preprocessor()
 
 st.title('Anime Recommendation')
 
@@ -24,17 +27,14 @@ anime_name = st.text_input('Enter the name of the anime')
 
 genres = st.text_input('Enter the genres (comma-separated)')
 
-model.summary()
-
 if st.button('Predict'):
     st.write('Anime Name:', anime_name)
     st.write('Genres:', genres)
 
-    input_dtype = model.layers[0].dtype
+    input_data = pd.DataFrame({'genre': [genres]})
+    preprocessed_data = preprocessor.transform(input_data)
 
-    input_data = preprocess_data(pd.DataFrame({'genre': [genres]}))
-    preprocessed_data = input_data.drop(columns=['genre'])
-    preprocessed_data = preprocessed_data.astype('float32')
+    preprocessed_data = preprocessed_data.drop(columns=['genre']).astype('float32')
 
     prediction = model.predict(preprocessed_data)
 
@@ -42,5 +42,25 @@ if st.button('Predict'):
     max_genre_name = preprocessed_data.columns[max_genre_index]
 
     movies_with_max_genre = data[data[max_genre_name] == 1]['name']
+
     st.header("Movies with the Predicted Genre:")
     st.write(movies_with_max_genre)
+
+#if st.button('Predict'):
+#    st.write('Anime Name:', anime_name)
+#    st.write('Genres:', genres)
+
+#    input_dtype = model.layers[0].dtype
+
+#    input_data = preprocess_data(pd.DataFrame({'genre': [genres]}))
+#    preprocessed_data = input_data.drop(columns=['genre'])
+#    preprocessed_data = preprocessed_data.astype('float32')
+
+#    prediction = model.predict(preprocessed_data)
+
+#    max_genre_index = prediction.argmax()
+#    max_genre_name = preprocessed_data.columns[max_genre_index]
+
+#    movies_with_max_genre = data[data[max_genre_name] == 1]['name']
+#    st.header("Movies with the Predicted Genre:")
+#    st.write(movies_with_max_genre)
