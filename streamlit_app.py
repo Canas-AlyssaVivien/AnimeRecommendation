e26ebf2748d528729635b26aad86951bb12f23ff
Alyssa Vivien S. Cañas <alyssavivien.sc@gmail.com>
2024-04-25 13:25:43 +0800

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,11 @@ if st.button('Predict'):
     #preprocessed_data = preprocessor.transform(input_data)
     #preprocessed_data = preprocessed_data.drop(columns=['genre']).astype('float32')
 
+    preprocessed_data = preprocessor.fit(input_data)
     preprocessed_data = preprocessor.transform(input_data)
+
+    st.write('PROCESSED: ', preprocessed_data)
+    
     preprocessed_data = preprocessed_data.drop(columns=['genre']).astype('float32')
 
     prediction = model.predict(preprocessed_data)
