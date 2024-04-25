2aba6b68ff0f5456a0132c7b0e4e9651159b8c08
Alyssa Vivien S. Cañas <alyssavivien.sc@gmail.com>
2024-04-25 14:35:52 +0800

    1

--- preprocessor.py
+++ preprocessor.py
@@ -5,11 +5,10 @@ from sklearn.preprocessing import MultiLabelBinarizer
 class Preprocessor(BaseEstimator, TransformerMixin):
     def __init__(self, all_genres):
         self.mlb = MultiLabelBinarizer(classes=all_genres)
-        self.fitted = False  # Track if the MultiLabelBinarizer has been fitted
+        self.fitted = False
 
     def fit(self, X, y=None):
-        # No need to fit the MultiLabelBinarizer
-        self.fitted = True  # Set fitted to True after initialization
+        self.fitted = True
         return self
 
     def transform(self, X):
@@ -21,14 +20,9 @@ class Preprocessor(BaseEstimator, TransformerMixin):
         
         X['genre'] = X['genre'].str.strip()
         
-        # Transform the genre column
         genres = self.mlb.transform(X['genre'].str.split(', '))
         genre_matrix = pd.DataFrame(genres, columns=self.mlb.classes_, index=X.index)
         
         X = pd.concat([X, genre_matrix], axis=1)
-        
-        # No need to limit the columns
-        # last_genre_column_index = 49
-        # X = X.iloc[:, :last_genre_column_index + 1]
-        
+               
         return X
--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,9 @@ if st.button('Predict'):
 
     preprocessed_data = preprocessor.fit(input_data)
     preprocessed_data = preprocessor.transform(input_data)
+
+    st.write('PreProcessed: ', preprocessed_data)
+
     preprocessed_data = preprocessed_data.drop(columns=['genre']).astype('float32')
 
     st.write('PreProcessed: ', preprocessed_data)
@@ -36,22 +39,3 @@ if st.button('Predict'):
 
     st.header("Movies with the Predicted Genre:")
     st.write(movies_with_max_genre)
-
-#if st.button('Predict'):
-#    st.write('Anime Name:', anime_name)
-#    st.write('Genres:', genres)
-
-#    input_dtype = model.layers[0].dtype
-
-#    input_data = preprocess_data(pd.DataFrame({'genre': [genres]}))
-#    preprocessed_data = input_data.drop(columns=['genre'])
-#    preprocessed_data = preprocessed_data.astype('float32')
-
-#    prediction = model.predict(preprocessed_data)
-
-#    max_genre_index = prediction.argmax()
-#    max_genre_name = preprocessed_data.columns[max_genre_index]
-
-#    movies_with_max_genre = data[data[max_genre_name] == 1]['name']
-#    st.header("Movies with the Predicted Genre:")
-#    st.write(movies_with_max_genre)
